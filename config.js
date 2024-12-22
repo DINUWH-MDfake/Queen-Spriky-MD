4e73dd6dd81b7b1e1ef9ba92937322c0185c9ea0
DINUWH-MDfake <dinuwahmd@gmail.com>
2024-12-22 10:32:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || '62oSGK5T#Uj8W3V9xitQl2qBoJHD8gSWI0DIed0XCvsDjB2NFy_Y', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94728896048',    // Enter Your Owner Number
 };
